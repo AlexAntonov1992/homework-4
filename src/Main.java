ba45fb1876c3cfcecb075079e3f7451b68978cd2
user <barbizylka@gmail.com>
2025-06-18 01:17:20 +0300

    дз4.11

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("привет, мир");
+        System.out.println("привет, мир мир");
 //Задача 1
 for (int i = 0; i <= 10; i = i + 1) {
     System.out.println(i);
