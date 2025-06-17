aab0d4fc7af1d02ca5eb6b41880702502688c602
user <barbizylka@gmail.com>
2025-06-18 01:10:32 +0300

    дз4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello,");
 //Задача 1
 for (int i = 0; i <= 10; i = i + 1) {
     System.out.println(i);
